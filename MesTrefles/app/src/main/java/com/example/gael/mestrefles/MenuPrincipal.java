bb440f132bc2e669503c19db7f5d624085c9c717
Gael <dimgael98@gmail.com>
2017-11-19 15:05:11 +0100

    Changement de la taille des boutons

--- MesTrefles/app/src/main/java/com/example/gael/mestrefles/MenuPrincipal.java
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/MenuPrincipal.java
@@ -1,9 +1,9 @@
 package com.example.gael.mestrefles;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MenuPrincipal extends AppCompatActivity {
+public class MenuPrincipal extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
