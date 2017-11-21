bb3cd949a1fc03420c3fe5ebc5a702ca7d6615e5
Gael <dimgael98@gmail.com>
2017-11-21 13:54:10 +0100

    Utilisation de la sidebar

--- MesTrefles/app/src/main/java/com/example/gael/mestrefles/MenuPrincipal.java
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/MenuPrincipal.java
@@ -8,6 +8,6 @@ public class MenuPrincipal extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_principal);
+        setContentView(R.layout.content_main);
     }
 }
