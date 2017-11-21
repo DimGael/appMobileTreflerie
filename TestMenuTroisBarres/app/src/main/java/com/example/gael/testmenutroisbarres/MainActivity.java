fb5c89fbf3c0e9add6aab91931e8b34cafeabad9
Gael <dimgael98@gmail.com>
2017-11-21 13:38:02 +0100

    Rajout de commentaires pour comprendre le code

--- TestMenuTroisBarres/app/src/main/java/com/example/gael/testmenutroisbarres/MainActivity.java
+++ TestMenuTroisBarres/app/src/main/java/com/example/gael/testmenutroisbarres/MainActivity.java
@@ -1,10 +1,6 @@
 package com.example.gael.testmenutroisbarres;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.TextViewCompat;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -22,24 +18,21 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
+        //Création du "Toogle" (les 3 barres en haut à gauche)
+        //On le relie au drawerLayout (le menu à gauche)
+        //Puis on l'affiche avec syncState()
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        //Sans la navigation View, on ne peut pas selectionner les item du menus à gauche
+        //Il faut que notre classe implemente l'interface : NavigationView.OnNavigationItemSelectedListener
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
