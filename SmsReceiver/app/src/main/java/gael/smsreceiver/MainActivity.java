bd6a277a13b777209563450f2f024965898bcf90
Gael <dimgael98@gmail.com>
2017-11-26 20:58:50 +0100

    Rajout du mini projet recevoir et lire les sms

---
+++ SmsReceiver/app/src/main/java/gael/smsreceiver/MainActivity.java
@@ -0,0 +1,14 @@
+package gael.smsreceiver;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.act);
+    }
+
+}
