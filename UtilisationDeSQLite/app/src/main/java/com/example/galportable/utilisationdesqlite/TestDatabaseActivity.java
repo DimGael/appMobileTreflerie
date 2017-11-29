7090d82c208d7e7b77df691a7229923183aa332b
DimGael <dimgael98@gmail.com>
2017-11-29 13:04:41 +0100

    Création de l'activité principale et de la classe MySQLiteHelper

--- UtilisationDeSQLite/app/src/main/java/com/example/galportable/utilisationdesqlite/TestDatabaseActivity.java
+++ UtilisationDeSQLite/app/src/main/java/com/example/galportable/utilisationdesqlite/TestDatabaseActivity.java
@@ -1,13 +1,14 @@
 package com.example.galportable.utilisationdesqlite;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class TestDatabaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
