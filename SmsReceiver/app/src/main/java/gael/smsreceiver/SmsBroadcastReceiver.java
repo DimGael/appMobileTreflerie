a08692177c92edf43e987cc2318f6d9f86bc0172
DimGael <dimgael98@gmail.com>
2017-11-30 13:24:26 +0100

    Commit du SMS Receiver qui marche
    Le sms Receiver ne reçoit les messages que de mon numéro : (+33687988327)

--- SmsReceiver/app/src/main/java/gael/smsreceiver/SmsBroadcastReceiver.java
+++ SmsReceiver/app/src/main/java/gael/smsreceiver/SmsBroadcastReceiver.java
@@ -9,29 +9,35 @@ import android.widget.Toast;
 
 public class SmsBroadcastReceiver extends BroadcastReceiver {
 
+
     public static final String SMS_BUNDLE = "pdus";
 
     public void onReceive(Context context, Intent intent) {
+        boolean doitAfficher = false;
         Bundle intentExtras = intent.getExtras();
         if (intentExtras != null) {
             Object[] sms = (Object[]) intentExtras.get(SMS_BUNDLE);
             String smsMessageStr = "";
+
             for (int i = 0; i < sms.length; ++i) {
                 SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) sms[i]);
 
                 String address = smsMessage.getOriginatingAddress();
 
-                if(address.equals("+33602298324")) {
+                if(address.equals("+33687988327")) {
                     String smsBody = smsMessage.getMessageBody().toString();
-                     smsMessageStr += "SMS From: " + address + "\n";
+                     smsMessageStr += "SMS Provenant de : " + address + "\n";
                     smsMessageStr += smsBody + "\n";
+                    doitAfficher = true;
                 }
             }
+            if(doitAfficher){
             Toast.makeText(context, smsMessageStr, Toast.LENGTH_SHORT).show();
 
             //this will update the UI with message
                 SmsActivity inst = SmsActivity.instance();
                 inst.updateList(smsMessageStr);
+            }
         }
     }
 }
