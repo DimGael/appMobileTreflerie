d417f8386d70a1092979bc3e1e007839394584a5
Alexandre Pommarat <alexandre.pommarat@etu.unilim.fr>
2017-11-30 13:27:24 +0100

    page d'accueil de l'appli
    pas encore finis , on essaye d'ajouter l'image

---
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/AccueilActivity.java
@@ -0,0 +1,32 @@
+package com.example.gael.mestrefles;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+/**
+ * Created by Alexandre and Quentin on 30/11/2017.
+ */
+
+public class AccueilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accueil);
+
+        Intent intentMenu = new Intent(this, MenuPrincipal.class);
+        this.startActivity(intentMenu);
+    }
+
+
+}
