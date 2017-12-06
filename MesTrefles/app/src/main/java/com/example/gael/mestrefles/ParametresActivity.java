8bbbc40ddc7ae7d6f6157c67dc7976932a6cd084
DimGael <dimgael98@gmail.com>
2017-12-06 14:28:34 +0100

    Mise à jour du montant max maintenant fonctionnel

--- MesTrefles/app/src/main/java/com/example/gael/mestrefles/ParametresActivity.java
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/ParametresActivity.java
@@ -12,15 +12,27 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.gael.sqlite.MontantMaxDataSource;
 
 
 public class ParametresActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener {
 
+    //Classe qui va nous servir à manipuler la table MontantMax
+    private MontantMaxDataSource montantMaxDataSource;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
+
+        this.montantMaxDataSource = new MontantMaxDataSource(this);
+        this.montantMaxDataSource.open();
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -33,6 +45,11 @@ public class ParametresActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //Ajout de l'écouteur sur le bouton
+        ((Button)this.findViewById(R.id.boutonValiderParametres)).setOnClickListener(this);
+
+        //Ajouter le montant actuel du montant max de transaction
 
     }
 
@@ -87,6 +104,47 @@ public class ParametresActivity extends AppCompatActivity
 
     @Override
     public void onClick(View view) {
+        int id = view.getId();
+        if(id == R.id.boutonValiderParametres){
+            //Action lorsque l'utilisateur appuie sur le bouton valider en paramètres
 
+            /*
+            1. gérer le fait que l'utilisateur n'ai rien mis
+            2. L'utilisateur entre une valeur supérieure à 250
+            */
+
+            final EditText editTextMontantMax  = (EditText)this.findViewById(R.id.editTextMontantMax);
+
+            if(editTextMontantMax.getText().toString().equals("")){
+                //Si l'utilisateur n'a rien mis :
+                Toast.makeText(this,"Vous n'avez rien saisi", Toast.LENGTH_SHORT).show();
+            }
+            else if(Double.valueOf(editTextMontantMax.getText().toString()).doubleValue() > 250.0){
+                //Si l'utilisateur a mis une valeur supérieure à 250
+                Toast.makeText(this,"Vous ne pouvez pas excéder 250 trèfles", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                //Si l'utilisateur a tout rempli correctement, mise à jour du montant maximal puis affichage du montant maximal
+                final double nouvMontantMax = Double.valueOf(editTextMontantMax.getText().toString()).doubleValue();
+
+                //Mise à jour du montant max
+                this.montantMaxDataSource.majMontantMax(nouvMontantMax);
+
+                //Affichage d'un message de confirmation rapelant le nouveau montant max
+                
+            }
+        }
+    }
+
+    @Override
+    public void onPause(){
+        this.montantMaxDataSource.close();
+        super.onPause();
+    }
+
+    @Override
+    public void onResume(){
+        this.montantMaxDataSource.open();
+        super.onResume();
     }
 }
