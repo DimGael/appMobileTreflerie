03a676c4946447a14af5aba784aaff936c5d4d3a
AlexisNadaud <alexisn87@hotmail.fr>
2017-12-09 18:56:46 +0100

    Ajout refus

---
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/RefusActivity.java
@@ -0,0 +1,26 @@
+package com.example.gael.mestrefles;
+
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Alexandre and Quentin on 30/11/2017.
+ */
+
+public class RefusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refus);
+    }
+
+}
