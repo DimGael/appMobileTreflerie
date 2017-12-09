3f04da7a96ed5091046d3a9541a09d4bd4ce0c20
Gael <dimgael98@gmail.com>
2017-12-09 15:09:24 +0100

    Création d'un mini projet pour demander des permissions à l'utilisateur

---
+++ DemandePermission/app/src/main/java/com/example/gael/demandepermission/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.gael.demandepermission;
+
+import android.Manifest;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.READ_SMS}, 123);
+        }
+    }
+}
