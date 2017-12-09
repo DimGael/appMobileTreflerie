bfbd76d3a940d349cd60c9e0aa98f2c0a99c6b23
AlexisNadaud <alexisn87@hotmail.fr>
2017-12-09 18:57:31 +0100

    4HDE RECH PERM API 23

--- MesTrefles/app/src/main/java/com/example/gael/mestrefles/AccueilActivity.java
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/AccueilActivity.java
@@ -1,10 +1,14 @@
 package com.example.gael.mestrefles;
 
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -19,19 +23,71 @@ import android.view.MenuItem;
  */
 
 public class AccueilActivity extends AppCompatActivity {
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intentMenu = new Intent(AccueilActivity.this, MenuPrincipal.class);
-                startActivity(intentMenu);
-                finish();
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.SEND_SMS)) {
+                //Cela signifie que la permission à déjà était
+                //demandé et l'utilisateur l'a refusé
+                //Vous pouvez aussi expliquer à l'utilisateur pourquoi
+                //cette permission est nécessaire et la redemander
+            } else {
+                //Sinon demander la permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.SEND_SMS},
+                        MY_PERMISSIONS_REQUEST_SEND_SMS);
             }
-        }, 3000);
+        }
+        else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intentMenu = new Intent(AccueilActivity.this, MenuPrincipal.class);
+                    startActivity(intentMenu);
+                    finish();
+                }
+            }, 3000);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[],
+                                           int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intentMenu1 = new Intent(AccueilActivity.this, MenuPrincipal.class);
+                            startActivity(intentMenu1);
+                            finish();
+                        }
+                    }, 3000);
+
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Intent intentMenu2 = new Intent(AccueilActivity.this, RefusActivity.class);
+                            startActivity(intentMenu2);
+                            finish();
+                        }
+                    }, 3000);
+                }
+                return;
+            }
+        }
     }
 
 
