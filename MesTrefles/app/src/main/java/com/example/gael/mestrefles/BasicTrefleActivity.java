7b5b4a8b41469dd21a0c31ef0c755960cae37f65
Alexandre Pommarat <alexandre.pommarat@etu.unilim.fr>
2018-01-22 16:26:46 +0100

    ajout logo toolbar

--- MesTrefles/app/src/main/java/com/example/gael/mestrefles/BasicTrefleActivity.java
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/BasicTrefleActivity.java
@@ -28,7 +28,7 @@ public abstract class BasicTrefleActivity extends AppCompatActivity implements N
     public static BasicTrefleActivity instance = null;
 
     public void majSoldeAffichage(double nouvSolde){
-        this.getToolbar().setTitle("Solde : "+this.soldeDataSource.getSoldeActuel());
+        this.getToolbar().setTitle("Solde : "+this.soldeDataSource.getSoldeActuel()+" Trèfles");
 
     }
 
@@ -38,14 +38,14 @@ public abstract class BasicTrefleActivity extends AppCompatActivity implements N
 
         this.soldeDataSource = new SoldeDataSource(this.getBaseContext());
         this.soldeDataSource.open();
-        this.getToolbar().setLogo(R.drawable.logo);
         this.numeroServeurDataSource = new NumeroServeurDataSource(this.getBaseContext());
         this.numeroServeurDataSource.open();
 
         instance = this;
 
         Toolbar toolbar = this.getToolbar();
-        toolbar.setTitle("Solde : "+this.soldeDataSource.getSoldeActuel());
+        toolbar.setTitle("Solde : "+this.soldeDataSource.getSoldeActuel()+" Trèfles");
+        toolbar.setLogo(R.drawable.logo);
         setSupportActionBar(toolbar);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
