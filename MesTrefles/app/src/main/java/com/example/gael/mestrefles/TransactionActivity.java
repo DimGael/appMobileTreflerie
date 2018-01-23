83323e7679f5eb914cedad81d6031c5ebf445d44
Alexandre Pommarat <alexandre.pommarat@etu.unilim.fr>
2018-01-23 16:24:53 +0100

    Revert "moins bug"
    This reverts commit e344eb3f2d5b9ccbccc3eb6ddfc326a3cfcda642.

--- MesTrefles/app/src/main/java/com/example/gael/mestrefles/TransactionActivity.java
+++ MesTrefles/app/src/main/java/com/example/gael/mestrefles/TransactionActivity.java
@@ -231,7 +231,8 @@ public class TransactionActivity extends BasicTrefleActivity
         this.transactionEnCours = false;
     }
 
-
+<<<<<<< HEAD
+=======
     public void transactionEchouee(){
         final TextView texteRep = (TextView)this.findViewById(R.id.texteReponseSolde);
         texteRep.setText("Transaction Echouée");
@@ -241,5 +242,5 @@ public class TransactionActivity extends BasicTrefleActivity
 
         this.transactionEnCours = false;
     }
-
+>>>>>>> 3fa73129ec7240bf03126579485bff14eec5639b
 }
