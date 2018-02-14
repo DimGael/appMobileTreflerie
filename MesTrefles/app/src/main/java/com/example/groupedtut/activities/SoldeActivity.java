b7fe7fd2f04ff1155083453187823304e5c8bb52
DimGael <gael.dim@etu.unilim.fr>
2018-02-14 11:11:03 +0100

    Modification du gitignore

--- MesTrefles/app/src/main/java/com/example/groupedtut/activities/SoldeActivity.java
+++ MesTrefles/app/src/main/java/com/example/groupedtut/activities/SoldeActivity.java
@@ -7,13 +7,11 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.Toolbar;
 import android.telephony.SmsManager;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
 
 import com.example.groupedtut.numerocompte.NumeroCompteDataSource;
 
