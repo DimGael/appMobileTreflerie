52a814111e39b7d9bd8cd068751609feaf260f83
DimGael <dimgael98@gmail.com>
2018-03-25 19:24:22 +0200

    débugage

--- MesTrefles/app/src/main/java/com/example/groupedtut/activities/SoldeActivity.java
+++ MesTrefles/app/src/main/java/com/example/groupedtut/activities/SoldeActivity.java
@@ -2,13 +2,11 @@ package com.example.groupedtut.activities;
 
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AlertDialog;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +15,6 @@ import android.widget.Toast;
 
 import com.example.groupedtut.expediteur_message.ExpediteurMessage;
 import com.example.groupedtut.expediteur_message.SMS.ExpediteurSMS;
-import com.example.groupedtut.expediteur_message.jabber.ExpediteurJabber;
 import com.example.groupedtut.expediteur_message.jabber.MyXMPP;
 import com.example.groupedtut.numerocompte.NumeroCompteDataSource;
 
@@ -42,8 +39,11 @@ public class SoldeActivity extends BasicTrefleActivity implements NavigationView
         super.onCreate(savedInstanceState);
 
         this.expediteurMessage = new ExpediteurSMS();
-        //this.expediteurMessage = new ExpediteurJabber(MyXMPP.globalMyXmpp);
+
+        /*Mode JABBER
+        this.expediteurMessage = new ExpediteurJabber(MyXMPP.globalMyXmpp);
         MyXMPP.globalMyXmpp.setMessageRecuListener(this);
+        */
 
         final Button boutonActualiser = (Button)this.findViewById(R.id.boutonActualiser);
         boutonActualiser.setOnClickListener(this);
