bb8f1b3f2cbd20915a27c9dcdeef12937ce2b438
DimGael <dimgael98@gmail.com>
2018-03-27 22:35:34 +0200

    Réparation de l'envoi de transaction

--- MesTrefles/app/src/main/java/com/example/groupedtut/expediteur_message/SMS/ExpediteurSMS.java
+++ MesTrefles/app/src/main/java/com/example/groupedtut/expediteur_message/SMS/ExpediteurSMS.java
@@ -6,7 +6,7 @@ import android.telephony.SmsManager;
 import com.example.groupedtut.expediteur_message.ExpediteurMessage;
 import com.example.groupedtut.numeroserveur.NumeroServeurDataSource;
 
-import java.math.BigDecimal;
+import java.util.regex.Pattern;
 
 /**
  * Created by Gael on 24/02/2018.
@@ -34,19 +34,11 @@ public class ExpediteurSMS implements ExpediteurMessage {
         numServeurDataSource.close();
     }
 
-    private String creerMessage(double montant, String numDestinataire){
-        BigDecimal bd = new BigDecimal(montant);
-        bd= bd.setScale(2,BigDecimal.ROUND_DOWN);
-        double montantArrondi = bd.doubleValue();
-
-        final int avantVirgule = (int) montantArrondi;
-
-        int indexVirgule = new Double(montantArrondi).toString().indexOf(".");
-        String apresVirgule = new Double(montantArrondi).toString().substring(indexVirgule + 1);
-
-        if (apresVirgule.equals("0"))
-            return avantVirgule+"/"+numDestinataire;
-        else
-            return avantVirgule + "," + apresVirgule + "/"+numDestinataire;
+    private String creerMessage(double montant, String numDestinataire) {
+        String str_montant = new Double(montant).toString();
+        String[] tab = str_montant.split(Pattern.quote("."));
+        System.out.println(tab.length);
+        System.out.println(tab[0]);
+        return numDestinataire + "/" + tab[0] + "," + tab[1];
     }
 }
